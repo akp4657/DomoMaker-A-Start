6828c6e0014a3920be07d0524809745955908437
Anthony Pichardo <anthonykpichardo@gmail.com>
2020-03-30 12:55:08 -0400

    Weird handlebars error

--- server/app.js
+++ server/app.js
@@ -8,6 +8,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const csrf = require('csurf');
+const redis = require('redis');
 
 const port = process.env.PORT || process.env.NODE_PART || 3000;
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
@@ -25,6 +29,25 @@ mongoose.connect(dbURL, mongooseOptions, (err) => {
   }
 });
 
+let redisURL = {
+  // You will need to follow the "Setting up Redis for Local Use" Instructions
+  hostname: 'redis-18523.c52.us-east-1-4.ec2.cloud.redislabs.com',
+  port: 18523,
+};
+
+let redisPass = 'zEHTAAn2Qfnad4HlvHZhu0q6kbE2FfYd';
+
+if (process.env.REDISVLOUD_URL) {
+  redisURL = url.parse(process.env.REDISVLOUD_URL);
+  redisPass = redisURL.auth.split(':')[1];
+}
+
+const redisClient = redis.createClient({
+  host: redisURL.hostname,
+  port: redisURL.port,
+  password: redisPass,
+});
+
 // Pull in our routes
 const router = require('./router.js');
 
@@ -33,19 +56,36 @@ const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
+
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
+
 app.use(session({
   key: 'sessionid',
+  store: new RedisStore({
+    client: redisClient,
+  }),
   secret: 'Domo Arigato',
   resave: true,
   saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+  },
 }));
+
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
+app.disable('x-powered-by');
 app.use(cookieParser());
+
+app.use(csrf());
+app.use((err, req, res, next) => {
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
+  console.log('Missing CSRF token');
+  return false;
+});
 
 router(app);
 
